89bbeeafb11e85fb18d63ff91112d124d47c89df
Nick Royer <hjmediastudios@gmail.com>
2012-03-10 18:52:53 -0600

    Added selection for highest target.

--- BBoxTargeting/Sources/TargetPositionDetermination.cpp
+++ BBoxTargeting/Sources/TargetPositionDetermination.cpp
@@ -11,11 +11,55 @@ using namespace std;
 
 void targetPositionDetermination::setTargetIndices(Target* targets, int* numTargets, IplImage* img)
 {
+	std::cout << "Number of targets: " << *numTargets << endl;
+
 	//Determine the size of the image
 	CvSize imgSize = cvGetSize(img);
+	int targetPossibilites[*numTargets][4];
+
 	int imgHeight = imgSize.height;
 	int imgWidth = imgSize.width;
 
-	std::cout << "Number of targets: " << *numTargets << endl;
+	int currentHighestY = 9000;
+	int currentLowestY = 0;
+
+	int highestTargetIndex = 0;
+	int lowestTargetIndex = 0;
+
+	for (int i=0; i<(*numTargets); i++)
+	{
+		//cycle through targets and determine if there are targets higher than the current target or lower
+		for (int j=0; j<(*numTargets); j++)
+		{
+			if (targets[i].getCenter().y < currentHighestY)
+			{
+				highestTargetIndex = i;
+				currentHighestY = targets[i].getCenter().y;
+			}
+
+			if (targets[i].getCenter().y > currentLowestY)
+			{
+				lowestTargetIndex = i;
+				currentLowestY = targets[i].getCenter().y;
+			}
+		}
+	}
+
+	for (int i= 0; i<4; i++)
+	{
+		targetPossibilites[highestTargetIndex][i] = 1;
+		targetPossibilites[lowestTargetIndex][i] = 0;
+	}
+
+	//print possibilities to debug
+	for (int i=0; i<(*numTargets); i++)
+	{
+		std::cout << "targets[" << i << "]: ";
+		for (int j=0; j<4; j++)
+		{
+			std::cout << targetPossibilites[i][j] << " ";
+		}
+		std::cout << endl;
+	}
 
 }
