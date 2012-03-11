604828467c1b86d1275ea8497379ddee28a00bd3
Nick Royer <hjmediastudios@gmail.com>
2012-03-10 18:16:35 -0600

    Created index selection source.

---
+++ BBoxTargeting/Sources/TargetPositionDetermination.cpp
@@ -0,0 +1,12 @@
+/*
+ * TargetPositionDetermination.cpp
+ *
+ *  Created on: Mar 10, 2012
+ *      Author: nick
+ */
+#include "../Headers/TargetPositionDetermination.h"
+
+void targetPositionDetermination::setTargetIndices()
+{
+
+}
