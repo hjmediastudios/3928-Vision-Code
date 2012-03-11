780f7c986a4d843eb8926898294fb8f6b12472e7
Nick Royer <hjmediastudios@gmail.com>
2012-03-10 18:16:20 -0600

    Created index selection header.

---
+++ BBoxTargeting/Headers/TargetPositionDetermination.h
@@ -0,0 +1,20 @@
+/*
+ * TargetPositionDetermination.h
+ *
+ *  Created on: Mar 10, 2012
+ *      Author: nick
+ */
+
+#ifndef TARGETPOSITIONDETERMINATION_H_
+#define TARGETPOSITIONDETERMINATION_H_
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+namespace targetPositionDetermination
+{
+	void setTargetIndices();
+}
+
+
+#endif /* TARGETPOSITIONDETERMINATION_H_ */
